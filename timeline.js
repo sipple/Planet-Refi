3878e7bb5024050089b69f3a8dbbe3b330f7d648
Eric Sipple <saalon@gmail.com>
2009-05-24 17:19:47 -0400

    Timeline technically functional.

--- timeline.js
+++ timeline.js
@@ -3,12 +3,13 @@
     var eventSource = new Timeline.DefaultEventSource();
     var bandInfos = [
     Timeline.createBandInfo({
-        eventsource:    eventSource,
+        eventSource:    eventSource,
         width:          "70%", 
         intervalUnit:   Timeline.DateTime.MONTH, 
         intervalPixels: 100
     }),
     Timeline.createBandInfo({
+        eventSource: eventSource,
         width:          "30%", 
         intervalUnit:   Timeline.DateTime.YEAR, 
         intervalPixels: 200
@@ -17,11 +18,9 @@
     bandInfos[1].syncWith = 0;
     bandInfos[1].highlight = true;
     
-    eventSource1.loadJSON('timeline.php', url);
-    
     tl = Timeline.create(document.getElementById("pmtimeline"), bandInfos);
     
-    tl.loadJSON("cubism.js", function(json, url) {eventSource.loadJSON(json, url);});
+    tl.loadJSON('timeline.php', function(json, url) { eventSource.loadJSON(json, url); });
   }
  
   var resizeTimerID = null;
--- timeline.php
+++ timeline.php
@@ -2,15 +2,18 @@
 
 include_once('library_main.php');
 
-print(json_encode(GetTimelineEvents()));
+print("{ \"dateTimeFormat\":\"iso8601\",  \"events\": ". str_replace("\"false\"","false", json_encode(GetTimelineEvents())) . "}");
 
 function GetTimelineEvents()
 {
     $dbh = SqlConnect();
     
-    $stmt = $dbh->prepare("SELECT username, entrydate, title, description
+    
+    $stmt = $dbh->prepare("SELECT from_unixtime(entrydate, get_format(datetime, 'ISO')) as start, title, description, 'false' AS durationEvent
                           FROM timeline t
                           JOIN users u ON t.userid = u.userid");
+    
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
     
     if ($stmt->execute())
         return $stmt->fetchAll();
