9f55cc70f6ef13a0c1f167fbc07f7e76dce97724
Eric Sipple <saalon@gmail.com>
2009-06-04 00:39:57 -0400

    Added a credit to myself in the footer. Classy.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
-<span style="font-size:smaller;"><p>Thank you to <a href="http://www.simile-widgets.org/">SIMILE Widgets</a> for the <a href="http://www.simile-widgets.org/timeline/">timeline</a>.
-Like the timeline, everything here is provided under the <a href="license.php">BSD License</a>.  Feel free to
-<a href="mailto:saalon@gmail.com">contact me</a> with any questions.</p></span>
+<span style="font-size:smaller;"><p><I>Race To Refinance</I> brought to you by <a href="http://saalonmuyo.com">Eric Sipple</a>.
+Thank you to <a href="http://www.simile-widgets.org/">SIMILE Widgets</a> for the <a href="http://www.simile-widgets.org/timeline/">timeline</a>.
+Like the timeline, everything here is provided under the <a href="license.php">BSD License</a>.</p>
 </body>
 </html>
